efe03fdf5171bb9fd06f98a56331c0f832f0ed80
duckdu555-oss <duckdu555@gmail.com>
2026-06-28 11:00:42 +0900

    Add files via upload

--- setting/setting.js
+++ setting/setting.js
@@ -1,1 +1,14 @@
+// 회원가입 코드 설정 파일
+// HTML 안이 아니라 여기에서만 코드를 설정하면 됩니다.
+// 예시: window.SIGNUP_CODE = "1357";
 
+window.SIGNUP_CODE = "";
+
+
+// 관리자 페이지 로그인 설정
+// 예시:
+// window.ADMIN_ID = "admin";
+// window.ADMIN_PASSWORD = "1234";
+
+window.ADMIN_ID = "";
+window.ADMIN_PASSWORD = "";
