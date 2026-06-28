9c99033088c349a59925afdfb40119094e3327d6
duckdu555-oss <duckdu555@gmail.com>
2026-06-28 11:41:46 +0900

    Update setting.js

--- setting/setting.js
+++ setting/setting.js
@@ -3,12 +3,12 @@
 // 예시: window.SIGNUP_CODE = "1357";
 
 // 회원가입 코드
-window.SIGNUP_CODE = "";
+window.SIGNUP_CODE = "0000";
 
 // 관리자 계정
-window.ADMIN_ID = "";
-window.ADMIN_PASSWORD = "";
+window.ADMIN_ID = "0000";
+window.ADMIN_PASSWORD = "0000";
 
 // 고객센터 텔레그램 주소
-// 예시: window.TELEGRAM_URL = "https://t.me/telegramid";
+// 예시: window.TELEGRAM_URL = "https://t.me/1234";
 window.TELEGRAM_URL = "";
